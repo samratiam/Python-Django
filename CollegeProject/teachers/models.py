28f65f0b192073faec53785a4179f59e8cef68ae
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-04-07 18:10:07 +0545

    Create and register teachers model

--- CollegeProject/teachers/models.py
+++ CollegeProject/teachers/models.py
@@ -1,3 +1,10 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+
+class Teachers(models.Model):
+    teacher_name = models.CharField(max_length=50)
+    teacher_age = models.IntegerField()
+    teacher_address = models.CharField(max_length=100)
