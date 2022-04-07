92109b3fb58538d52d0de7fd3514c00564ca62ed
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-04-07 18:44:33 +0545

    Fix import model name

--- CollegeProject/courses/models.py
+++ CollegeProject/courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from teachers.models import Teachers
+from .models import Teacher
 # Create your models here.
 
 
@@ -7,4 +7,4 @@ class Course(models.Model):
     course_name = models.CharField(max_length=80)
     course_code = models.CharField(max_length=10)
     course_faculty = models.CharField(max_length=50)
-    course_teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE)
+    course_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
