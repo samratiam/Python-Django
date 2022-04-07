2972110f40a394a6543ee0be5f923dff95d5e472
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-04-07 18:49:52 +0545

    Rename teacher and course model

--- CollegeProject/courses/models.py
+++ CollegeProject/courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .models import Teacher
+from teachers.models import Teacher
 # Create your models here.
 
 
--- CollegeProject/teachers/admin.py
+++ CollegeProject/teachers/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Teachers
+from .models import Teacher
 # Register your models here.
-admin.site.register(Teachers)
+admin.site.register(Teacher)
