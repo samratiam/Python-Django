9b98de667ffc3d900d10947458d92bdc32bdacbf
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-04-07 18:27:13 +0545

    Create and register courses model

--- CollegeProject/courses/models.py
+++ CollegeProject/courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from teachers.models import Teachers
 # Create your models here.
+
+
+class Courses(models.Model):
+    course_name = models.CharField(max_length=80)
+    course_code = models.CharField(max_length=10)
+    course_faculty = models.CharField(max_length=50)
+    course_teacher = models.ForeignKey(Teachers, on_delete=models.CASCADE)
