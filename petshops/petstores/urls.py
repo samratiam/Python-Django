044a6e12e87aa636880b19aa2be97bee2fc83821
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-05-05 15:21:35 +0545

    Petstore CRUD API using Viewset

--- petshops/petstores/urls.py
+++ petshops/petstores/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path,include
-from .views import LocationViewSet
+from .views import LocationViewSet,PetstoreViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'locations',LocationViewSet,basename='location')
-
+router.register(r'petstores',PetstoreViewSet,basename='petstore')
 urlpatterns = [
     path("",include(router.urls)),
 ]
--- petshops/petstores/views.py
+++ petshops/petstores/views.py
@@ -51,3 +51,46 @@ class LocationViewSet(viewsets.ViewSet):
         location = Location.objects.get(pk=id)
         location.delete()
         return Response({'msg':'Data deleted'})
+
+class PetstoreViewSet(viewsets.ViewSet):
+    def create(self, request):
+        serializer = PetstoreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Data created'},status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def list(self, request):
+        queryset = Petstore.objects.all()
+        serializer = PetstoreSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk = None):
+        queryset = Petstore.objects.all()
+        petstore = get_object_or_404(queryset, pk=pk)
+        serializer = PetstoreSerializer(petstore)
+        return Response(serializer.data)
+
+    def update(self,request,pk):
+        id = pk
+        petstore = Petstore.objects.get(pk=id)
+        serializer = PetstoreSerializer(petstore, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Complete data updated'})
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def partial_update(self,request,pk):
+        id = pk
+        petstore = Petstore.objects.get(pk=id)
+        serializer = PetstoreSerializer(petstore, data = request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'Partial data updated'})
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)     
+
+    def destroy(self,request,pk):
+        id = pk
+        petstore = Petstore.objects.get(pk=id)
+        petstore.delete()
+        return Response({'msg':'Data deleted'})
