df3ccb8908e64b29489cff1cb61ea9a9d1a53a63
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-09-09 00:10:42 +0545

    Fix settings

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -140,9 +140,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_PORT = '2525'
 
 # lakovarmmnfdxwqc
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pudasaini.samrat@gmail.com'
-EMAIL_HOST_PASSWORD = 'lakovarmmnfdxwqc'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'pudasaini.samrat@gmail.com'
+# EMAIL_HOST_PASSWORD = 'lakovarmmnfdxwqc'
